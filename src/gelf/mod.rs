b334229d241623e2c6e56d42d2dd14783eee2356
Stephane Benoit <stefb@wizzz.net>
2023-10-16 20:04:25 +0200

    manage compressed or uncompressed gelf message

--- src/gelf/mod.rs
+++ src/gelf/mod.rs
@@ -4,11 +4,18 @@ use std::io::Read;
 
 pub type GelfMessage = HashMap<String, serde_json::Value>;
 
-pub fn decode_gelf_message(compressed_data: &[u8]) -> Result<GelfMessage, Box<dyn std::error::Error>> {
-    let mut d = GzDecoder::new(compressed_data);
-    let mut decompressed_data = String::new();
-    d.read_to_string(&mut decompressed_data)?;
+fn is_gzipped(data: &[u8]) -> bool {
+    data.starts_with(&[0x1f, 0x8b])
+}
 
-    let message: GelfMessage = serde_json::from_str(&decompressed_data)?;
+pub fn decode_gelf_message(data: &[u8]) -> Result<GelfMessage, Box<dyn std::error::Error>> {
+    let mut _data = String::new();
+    if is_gzipped(&data) {
+        let mut d = GzDecoder::new(data);
+        d.read_to_string(&mut _data)?;    
+    } else {
+        _data = String::from_utf8_lossy(&data).to_string();
+    }
+    let message: GelfMessage = serde_json::from_str(&_data)?;
     Ok(message)
 }
